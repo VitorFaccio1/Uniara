07cdc90f2e37702e69fc3efb62b80c8579830fa3
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2022-02-23 22:22:52 -0300

    Update main.c

--- 2ºAno/1ºSemestre/C/Aula03/main.c
+++ 2ºAno/1ºSemestre/C/Aula03/main.c
@@ -32,45 +32,47 @@ int main(int argc, char *argv[]) {
 	
 	do{
 		
-    	printf("####################\n");
-    	printf("1)Somar\n2)Subtrair\n3)Multiplicar\n4)Dividir\n5)Sair\n");
-    	printf("####################\n");
-    	printf("Digite a operacao: ");
-    	scanf("%d", &operacao);
+		printf("\nCalculadora\n");
+		printf("1)Somar\n2)Subtrair\n3)Multiplicar\n4)Dividir\n5)Sair\n");
+		printf("Digite a operacao: ");
+		scanf("%d", &operacao);
+
     	
-    	
-    	if(operacao != 5){
-    	    printf("Digite o primeiro numero: ");
-        	scanf("%f", &a);
-        	
-        	printf("Digite o segundo numero: ");
-        	scanf("%f", &b);   
-    	}
+		if(operacao < 5){
+			printf("Digite o primeiro numero: ");
+			scanf("%f", &a);
+
+			printf("Digite o segundo numero: ");
+			scanf("%f", &b);   
+    		}
 	
-    	switch(operacao){
-    		case 1: 
-    			soma = somar(a,b);
-    			printf("%2.f\n", soma);
-    			return 0;
-    			break;
-    		case 2: 
-    			subtrai = subtrair(a,b);
-    			printf("%2.f\n", subtrai);
-    			break;
-    		case 3: 
-    			multiplica = multiplicar(a,b);
-    			printf("%2.f\n", multiplica);
-    			break;
-    		case 4: 
-    			divide = dividir(a,b);
-    			if(divide != -100)
-    			    printf("%2.f\n", divide);
-    			else
-    			    printf("Impossivel dividir por zero, tente novamente!\n");
-    			break;
-    	}	
+		switch(operacao){
+			case 1: 
+				soma = somar(a,b);
+				printf("Resultado soma: %.2f\n", soma);
+				break;
+			case 2: 
+				subtrai = subtrair(a,b);
+				printf("Resultado subtracao: %.2f\n", subtrai);
+				break;
+			case 3: 
+				multiplica = multiplicar(a,b);
+				printf("Resultado multiplicacao: %.2f\n", multiplica);
+				break;
+			case 4: 
+				divide = dividir(a,b);
+				if(divide != -100)
+				    printf("Resultado divisao: %.2f\n", divide);
+				else
+				    printf("Impossivel dividir por zero, tente novamente!\n");
+				break;
+			case 5:
+			    break;
+			default: 
+			    printf("\nInsira um numero correto\n");
+			    break;
+		}	
 	}while(operacao != 5);
 
 	return 0;
 }
-	
